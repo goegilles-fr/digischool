e1f956d8bfdb05d71270a01f8dcfbb74651f58ab
okoburte <jburte34@gmail.com>
2025-11-20 17:02:38 +0100

    update prisma repository prof et classe

--- src/repositories/classeRepository.js
+++ src/repositories/classeRepository.js
@@ -1,46 +1,50 @@
-let classeList = [
-    { id:1, nom:"D02", profId:1 },
-    { id:2, nom:"D04", profId:2 },
-    { id:3, nom:"D03", profId:2 },
-    { id:4, nom:"C01", profId:4 },
-    { id:5, nom:"C06", profId:1 },
-    { id:6, nom:"C04", profId:3 },
-    { id:7, nom:"B07", profId:4 },
-    { id:8, nom:"B03", profId:3 },
-    { id:9, nom:"A05", profId:1 }
-];
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-exports.getAllClasse = () => {
-    return classeList;
-}
+// Récupérer toutes les classes
+exports.getAllClasse = async () => {
+  return await prisma.classe.findMany();
+};
 
-exports.getClasseById = (id) => {
-    return classeList.find((c) => c.id === id) || null;
-}
+// Récupérer une classe par id
+exports.getClasseById = async (id) => {
+  return await prisma.classe.findUnique({
+    where: { id: id },
+  });
+};
 
-exports.createClasse = (classe) => {
-    const maxId = classeList.length ? Math.max( ...classeList.map((c) => c.id)) : 0;
-    const newClasse = { ...classe, id: maxId + 1 };
+// Créer une classe
+exports.createClasse = async (classe) => {
+  return await prisma.classe.create({
+    data: {
+      nom: classe.nom,
+      profId: classe.profId,
+    },
+  });
+};
 
-    classeList.push(newClasse);
+// Mettre à jour une classe
+exports.updateClasse = async (classe) => {
+  return await prisma.classe.update({
+    where: { id: classe.id },
+    data: {
+      nom: classe.nom,
+      profId: classe.profId,
+    },
+  });
+};
 
-    return newClasse;
-}
-
-exports.updateClasse = (classe) => {
-    const index = classeList.findIndex((c) => c.id === classe.id);
-
-    if(index === -1) return null;
-
-    classeList[index] = { ...classeList[index], ...classe };
-    return classeList[index];
-}
-
-exports.deleteClasse = (id) => {
-    const updatedClassList = classeList.filter((c) => c.id !== id);
-
-    const deleted = updatedClassList.length !== classeList.length;
-    if(deleted) classeList = updatedClassList;
-
-    return deleted;
-}
+// Supprimer une classe
+exports.deleteClasse = async (id) => {
+  try {
+    await prisma.classe.delete({
+      where: { id: id },
+    });
+    return true;
+  } catch (error) {
+    if (error.code === 'P2025') {
+      return false;
+    }
+    throw error;
+  }
+};
