6bdcd61d051a0e4dd351a3fa4e654ae1409d2647
Tommy-BOU <tommy.boubaker@free.fr>
2025-11-21 14:14:24 +0100

    add swagger annotations for classeRoutes and profRoutes

--- src/routes/profRoutes.js
+++ src/routes/profRoutes.js
@@ -3,10 +3,139 @@ const router = express.Router();
 
 const profController = require('../controllers/profController');
 
+/**
+ * @openapi
+ * /api/profs:
+ *   get:
+ *     tags:
+ *       - Professors
+ *     summary: Get all professors
+ *     responses:
+ *       200:
+ *         description: A list of professors
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ */
 router.get('/profs', profController.getAllProf);
+
+/**
+ * @openapi
+ * /api/profs/{id}:
+ *   get:
+ *     tags:
+ *       - Professors
+ *     summary: Get a professor by ID
+ *     responses:
+ *       200:
+ *         description: A professor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
 router.get('/profs/:id', profController.getProfById);
+
+/**
+ * @openapi
+ * /api/profs:
+ *   post:
+ *     tags:
+ *       - Professors
+ *     summary: Create a new professor
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nom:
+ *                 type: string
+ *               prenom:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               date_naissance:
+ *                 type: string
+ *               date_embauche:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: The created professor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
 router.post('/profs', profController.createProf);
+
+/**
+ * @openapi
+ * /api/profs/{id}:
+ *   put:
+ *     tags:
+ *       - Professors
+ *     summary: Update a professor by ID
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID of the professor to update
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nom:
+ *                 type: string
+ *               prenom:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               date_naissance:
+ *                 type: string
+ *               date_embauche:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: The updated professor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
 router.put('/profs/:id', profController.updateProf);
+
+/**
+ * @openapi
+ * /api/profs/{id}:
+ *   delete:
+ *     tags:
+ *       - Professors
+ *     summary: Delete a professor by ID
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID of the professor to delete
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: The deleted professor
+*/
 router.delete('/profs/:id', profController.deleteProf);
 
 module.exports = router;
